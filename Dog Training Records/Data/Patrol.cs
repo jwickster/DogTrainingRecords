4c97032b58138008adb6c447002b77dee0660ad7
Josh Wickster <joshwickster@gmail.com>
2024-01-19 13:15:36 -0700

    Update Patrol.cs

--- Dog Training Records/Data/Patrol.cs
+++ Dog Training Records/Data/Patrol.cs
@@ -9,6 +9,17 @@ public class Patrol
     ushort Humidity { get; set; }
     ushort DecoySetTime { get; set; }
     ushort DecoyFindTime { get; set; }
-    
-    
+
+    uint BuildingID { get; set; }
+    string BuildingName { get; set; }
+    string BuildingType { get; set; }
+    string BuildingStatus { get; set; }
+    string BuildingLocation { get; set; }
+    string BuildingOwner { get; set; }
+    string BuildingOwnerEmail { get; set; }
+    string BuildingOwnerPhone { get; set; }
+    string BuildingManager { get; set; }
+
+
+
 }
